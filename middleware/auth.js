9f49ae28bba4e3864d05106a2c6bf33b118dfdfc
Saurabh Thakur <saurabh7481@gmail.com>
2021-08-13 21:01:30 +0530

    Final Commit

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-const config = require('config');
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 module.exports = function (req, res, next) {
   // Get token from header
@@ -7,25 +7,24 @@ module.exports = function (req, res, next) {
   let token = "";
   // Check if not token
   if (header) {
-    token = req.headers.authorization.split(' ')[1];
-  } else if(req.header('x-auth-token') !== null){
-    token = req.header('x-auth-token');
+    token = req.headers.authorization.split(" ")[1];
+  } else if (req.header("x-auth-token") !== null) {
+    token = req.header("x-auth-token");
   } else {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   // Verify token
   try {
-    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+    jwt.verify(token, config.get("jwtSecret"), (error, decoded) => {
       if (error) {
-        return res.status(401).json({ msg: 'Token is not valid' });
+        return res.status(401).json({ msg: "Token is not valid" });
       } else {
         req.user = decoded.user;
         next();
       }
     });
   } catch (err) {
-    console.error('something wrong with auth middleware');
-    res.status(500).json({ msg: 'Server Error' });
+    res.status(500).json({ msg: "Server Error" });
   }
 };
